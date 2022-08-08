7ef3f872a25bdbbdc5544b0fe01bc3efdf45e405
Henrik Grimler <grimler@termux.dev>
2022-08-08 12:11:48 +0200

    fix(root/lvm2): add libblkid to dependencies

--- root-packages/lvm2/build.sh
+++ root-packages/lvm2/build.sh
@@ -3,10 +3,10 @@ TERMUX_PKG_DESCRIPTION="A device-mapper library from LVM2 package"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=2.03.16
-TERMUX_PKG_REVISION=1
+TERMUX_PKG_REVISION=2
 TERMUX_PKG_SRCURL=https://mirrors.kernel.org/sourceware/lvm2/releases/LVM2.${TERMUX_PKG_VERSION}.tgz
 TERMUX_PKG_SHA256=e661ece15b5d88d8abe39a4c1e1db2f43e1896f019948bb98b0e15d777680786
-TERMUX_PKG_DEPENDS="libandroid-support, libaio, readline"
+TERMUX_PKG_DEPENDS="libandroid-support, libblkid, libaio, readline"
 TERMUX_PKG_BREAKS="libdevmapper-dev"
 TERMUX_PKG_REPLACES="libdevmapper-dev"
 
