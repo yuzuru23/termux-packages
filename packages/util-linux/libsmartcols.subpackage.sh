6ef911c76e0ba67071e2cbf0f76a2037f4744caf
Henrik Grimler <grimler@termux.dev>
2022-08-08 11:23:17 +0200

    enhance(main/util-linux): move libsmartcols to new subpackage
    Quite a few util-linux utilities link against libsmartcols only.  Move
    the library into a subpackage so that other utilities can depend on
    libsmartcols, and not the full util-linux package.

---
+++ packages/util-linux/libsmartcols.subpackage.sh
@@ -0,0 +1,7 @@
+TERMUX_SUBPKG_DESCRIPTION="Library for smart adaptive formatting of tabular data"
+TERMUX_SUBPKG_DEPEND_ON_PARENT="no"
+TERMUX_SUBPKG_INCLUDE="
+lib/libsmartcols.so
+lib/pkgconfig/smartcols.pc
+include/libsmartcols/libsmartcols.h
+"
